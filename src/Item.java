b45ed609a74dc69d0b563a63c3ad9984190f1e64
Almutairi0 <Almutairi.swe@gmail.com>
2025-12-08 13:53:29 +0300

    Add Item and Node classes as base data structures

---
+++ src/Item.java
@@ -0,0 +1,44 @@
+public class Item {
+    private String  sku, name, category;
+    private double price;
+    private int quantity;
+
+    public Item(String sku, String name, String category, double price, int quantity) {
+        this.sku = sku;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getSKU() {
+        return sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int qty) {
+        this.quantity = qty;
+    }
+
+
+
+}
---
+++ src/Node.java
@@ -0,0 +1,9 @@
+class Node {
+    Item item;
+    Node next;
+    
+    Node(Item item) {
+        this.item = item;
+        this.next = null;
+    }
+}
