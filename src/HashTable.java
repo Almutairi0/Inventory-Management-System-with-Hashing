da323507c2e68e91e090c8b17dbe82e72e70c69f
Almutairi0 <Almutairi.swe@gmail.com>
2025-12-12 01:05:27 +0300

    Add complete HashTable implementation and integrate with existing LinkedList

---
+++ src/HashTable.java
@@ -0,0 +1,52 @@
+public class HashTable {
+	private LinkedList[] buckets;
+	private int size;
+
+	public HashTable(int size) {
+		this.size = size;
+		buckets = new LinkedList[size];
+		for (int i = 0; i < size; i++) {
+			buckets[i] = new LinkedList();
+		}
+	}
+
+	public int hash(String sku) {
+		int hashValue = sku.hashCode();
+		hashValue = Math.abs(hashValue);
+		return hashValue % size;
+	}
+
+	public void insert(Item item) {
+		int position = hash(item.getSKU());
+		buckets[position].insert(item);
+	}
+
+	public boolean delete(String sku) {
+		int position = hash(sku);
+		return buckets[position].delete(sku);
+	}
+
+	public Item search(String sku) {
+		int position = hash(sku);
+		return buckets[position].search(sku);
+	}
+
+	public void update(Item newItem) {
+		int position = hash(newItem.getSKU());
+
+		Item existing = buckets[position].search(newItem.getSKU());
+
+		if (existing != null) {
+			existing.setPrice(newItem.getPrice());
+			existing.setQuantity(newItem.getQuantity());
+		}
+	}
+
+	public void displayAll() {
+		for (int i = 0; i < size; i++) {
+			System.out.print("Bucket " + i + ": ");
+			buckets[i].display();
+		}
+	}
+
+}
